594f4ff7189acf0891a03c6d18bceaa1727ddf33
Jerald Rivera <jlrivera@gofluent.com>
2019-06-23 23:02:20 +0800

    UI done

--- src/Components/CounterInput/CounterInput.js
+++ src/Components/CounterInput/CounterInput.js
@@ -5,11 +5,26 @@ import './CounterInput.scss';
 const CounterInput = ({ handleCounterSubmit }) => {
   const [counter, setCounter] = useState({ title: '' });
 
-  const handleInputChange = (e) => setCounter({ title: e.target.value });
+  const handleInputChange = (e) => {
+    setCounter({ title: e.target.value });
+  };
+
+  const handleSubmit = () => {
+    handleCounterSubmit(counter);
+    setCounter({ title: '' });
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      handleSubmit(counter);
+    }
+  };
 
   return (
     <div className="counter-input-container">
       <input
+        value={counter.title}
+        onKeyPress={handleKeyPress}
         placeholder="quick! add a counter here :)"
         className="counter-input"
         onChange={handleInputChange}
@@ -18,7 +33,7 @@ const CounterInput = ({ handleCounterSubmit }) => {
         className="counter-add"
         src={add}
         alt="plus"
-        onClick={() => handleCounterSubmit(counter)}
+        onClick={() => handleSubmit(counter)}
       />
     </div>
   );
